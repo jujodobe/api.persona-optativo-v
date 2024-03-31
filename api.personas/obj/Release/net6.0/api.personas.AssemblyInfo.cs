a2d2897a9feed209822fad9985e84535f86ece60
d4v0x <davichomil@gmail.com>
2024-03-31 16:44:14 -0400

    Prueba

--- api.personas/obj/Release/net6.0/api.personas.AssemblyInfo.cs
+++ api.personas/obj/Release/net6.0/api.personas.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.personas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4884412ffe1c3cdbff003ffdbb8bd07cddbc3e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.personas")]
