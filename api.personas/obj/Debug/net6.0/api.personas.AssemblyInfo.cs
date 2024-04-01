9ea1ccd72f5ce3620cc34293dac50cfe46e3e365
Ioav <ioavbrunstein@gmail.com>
2024-03-31 20:01:21 -0400

    Se implementaron los metodos faltantes

--- api.personas/obj/Debug/net6.0/api.personas.AssemblyInfo.cs
+++ api.personas/obj/Debug/net6.0/api.personas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.personas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4884412ffe1c3cdbff003ffdbb8bd07cddbc3e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.personas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.personas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
